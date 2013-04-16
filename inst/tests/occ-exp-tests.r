a073da7d8d5f57320c84f30111fc4aa5fc86b03f
dennis feehan <dfeehan@princeton.edu>
2013-04-15 23:39:31 -0400

    skeleton file for unit tests for occ/exp code

---
+++ inst/tests/occ-exp-tests.r
@@ -0,0 +1,13 @@
+## TODO -- put tests of occurrence-exposure functions here
+
+## test exp.in.interval
+
+## test make.lifeline
+
+## test events.in.interval
+
+## test compute.occ.exp
+##  -> be sure to test both cases where
+##     - times are absolute
+##     - times are windows before row-specific interview date
+##  -> be sure to also test weights
