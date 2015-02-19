21a2e46e1fc5e0ab96e5149fc6a2a41545ebb8aa
dennis feehan <dfeehan@princeton.edu>
2015-02-19 11:38:28 -0500

    changed comments so we can use r/doxygen
    this is described in more detail here:
    http://blog.ironholds.org/adding-rcpp-to-an-existing-r-package-documented-with-roxygen2/
    (it's not clear whether or not the /* */ style comments would also work)

--- src/cpp_compute_occ_exp.cpp
+++ src/cpp_compute_occ_exp.cpp
@@ -7,25 +7,25 @@
 
 using namespace Rcpp;
 
-/*
- * window_intersect
- *
- * given two intervals, figure out the intersection between the
- * first and the second
- *
- * NOTE that the intervals are treated as half-open (start,finish]
- * so that an event that happens exactly at time b is counted in
- * (a,b] but not in (b, c]
- *
- * @param a the first window; here, we use all three components
- *              (start, finish, event)
- * @param b the second window; here, we only use the start and finish
- *              time, and ignore any events
- * @return a window containing the exposure and events from a that takes place
- *         during the time given by b; also, if a has an event and it takes
- *         place during b, it is included
- *
- */
+//'
+//' window_intersect
+//'
+//' given two intervals, figure out the intersection between the
+//' first and the second
+//'
+//' NOTE that the intervals are treated as half-open (start,finish]
+//' so that an event that happens exactly at time b is counted in
+//' (a,b] but not in (b, c]
+//'
+//' @param a the first window; here, we use all three components
+//'              (start, finish, event)
+//' @param b the second window; here, we only use the start and finish
+//'              time, and ignore any events
+//' @return a window containing the exposure and events from a that takes place
+//'         during the time given by b; also, if a has an event and it takes
+//'         place during b, it is included
+//'
+//'
 // [[Rcpp::export]]
 NumericVector window_intersect(NumericVector a, NumericVector b)
 {
